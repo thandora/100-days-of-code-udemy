a424bee16e7c0b219ee985a054a3945774d22ca9
thandora <51294367+thandora@users.noreply.github.com>
2023-01-24 21:53:31 +0800

    Added replayability and scoring system

--- Day 004 Rock Paper Scissors/main.py
+++ Day 004 Rock Paper Scissors/main.py
@@ -28,35 +28,69 @@ scissors = '''
 
 # Write your code below this line 👇
 choices = [rock, paper, scissors]
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-bot_choice = random.randint(0, 2)
-print(choices[user_choice])
-print(f"Computer chose:\n{choices[bot_choice]}")
+game_running = True
+score = {"wins": 0,
+        "loses": 0,
+        "draws": 0}
 
-# If user chooses rock.
-if user_choice == 0:
-    if bot_choice == 0:     # Bot chooses rock
-        print("It's a tie!")
-    elif bot_choice == 1:   # Bot chooses paper
-        print("You lose!")
-    elif bot_choice == 2:   # Bot chooses scissors
-        print("You win!")
+while game_running:
+    user_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. \"x\" to end\n").lower()
+    if user_choice == "x":
+        game_running = False
+        break
+
+    user_choice = int(user_choice)
+
+    bot_choice = random.randint(0, 2)
+    print(choices[user_choice])
+    print(f"Computer chose:\n{choices[bot_choice]}")
 
 
-# User chooses paper.
-elif user_choice == 1:
-    if bot_choice == 0:     # Bot chooses rock
-        print("You win!")
-    elif bot_choice == 1:   # Bot chooses paper
-        print("It's a tie!")
-    elif bot_choice == 2:   # Bot chooses scissors
-        print("You lose!")
+    # If user chooses rock.
+    if user_choice == 0:
+        if bot_choice == 0:     # Bot chooses rock
+            print("It's a tie!")
+            score["draws"] += 1
+        elif bot_choice == 1:   # Bot chooses paper
+            print("You lose!")
+            score["loses"] += 1
+        elif bot_choice == 2:   # Bot chooses scissors
+            print("You win!")
+            score["wins"] += 1
 
-# User chooses scissors.
-elif user_choice == 2:
-    if bot_choice == 0:     # Bot chooses rock
-        print("You lose!")
-    elif bot_choice == 1:   # Bot chooses paper
-        print("You win!")
-    elif bot_choice == 2:   # Bot chooses scissors
-        print("It's a tie!")
+
+    # User chooses paper.
+    elif user_choice == 1:
+        if bot_choice == 0:     # Bot chooses rock
+            print("You win!")
+            score["wins"] += 1
+        elif bot_choice == 1:   # Bot chooses paper
+            print("It's a tie!")
+            score["draws"] += 1
+        elif bot_choice == 2:   # Bot chooses scissors
+            print("You lose!")
+            score["loses"] += 1
+
+    # User chooses scissors.
+    elif user_choice == 2:
+        if bot_choice == 0:     # Bot chooses rock
+            print("You lose!")
+            score["loses"] += 1
+        elif bot_choice == 1:   # Bot chooses paper
+            print("You win!")
+            score["wins"] += 1
+        elif bot_choice == 2:   # Bot chooses scissors
+            print("It's a tie!")
+            score["draws"] += 1
+
+    score_sum = 0
+    for _ in score.values():
+        score_sum += _
+
+    winrate = score["wins"] / (score_sum)
+    
+    print(f"""Wins: {score["wins"]}
+Loses: {score["loses"]}
+Draws: {score["draws"]}
+win rate:{winrate * 100:.2f}%""")
+    print("-"*30)
