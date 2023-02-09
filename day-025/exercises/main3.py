ba60d345b7b0a0b8e0589dac09e01b12f9f3b2c6
Christian Klint Labadia <cklabadia@hotmail.com>
2023-02-10 06:27:49 +0800

    First working version

--- day-025/exercises/main3.py
+++ day-025/exercises/main3.py
@@ -4,20 +4,23 @@ import pandas as pd
 # of the primary fur color of squirrels
 
 # All data
-df = pd.read_csv("squirrel_central_park.csv")
-
+data = pd.read_csv("squirrel_central_park.csv")
 
 colors = ["Gray", "Cinnamon", "Black"]
 n_colors = {"Fur Color": [], "Count": []}
 
 for color in colors:
     # Select all rows that matches queried color
-    column = df[df["Primary Fur Color"] == color]
+    column = data[data["Primary Fur Color"] == color]
+
+    n = len(column)
+    # or by use of count method
     # Count how many rows on query
-    n = column.count(axis=0)["Primary Fur Color"]
+    n_2 = column.count(axis=0)["Primary Fur Color"]
+
     # Append to dictionary
     n_colors["Fur Color"].append(color)
     n_colors["Count"].append(n)
 
-# Convert 
+# Convert
 colors_csv = pd.DataFrame(n_colors).to_csv("colors.csv")
