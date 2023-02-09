f66e9cdb6b64e0eccfad8cb8f8a4852c7a22e49d
Christian Klint Labadia <cklabadia@hotmail.com>
2023-02-10 07:07:35 +0800

    Changed dir

---
+++ day-025/us-states-game/main.py
@@ -0,0 +1,15 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+screen.addshape("black_states.gif")
+
+turtle.shape("blank_states.gif")
+
+
+
+
+
+
+screen.exitonclick()
