82ca6ff0dbb75634c3a22ec22f155ca49d39b7f3
Christian Klint Labadia <cklabadia@hotmail.com>
2023-02-12 11:23:04 +0800

    Initial commit

---
+++ day-028/pomodoro/main.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title("Pomodoro")
+# window.minsize(width=800, height=500)
+canvas = tk.Canvas(width=200, height=224)
+tomato_img = tk.PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.pack()
+window.mainloop()
