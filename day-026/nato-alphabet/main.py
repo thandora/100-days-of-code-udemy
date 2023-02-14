35758ace5d357c73be7075f020c239d4dc0a13f5
Christian Klint Labadia <cklabadia@hotmail.com>
2023-02-14 13:30:29 +0800

    Rephrased input prompt

--- day-026/nato-alphabet/main.py
+++ day-026/nato-alphabet/main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import pprint
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
@@ -8,7 +7,7 @@ data = pd.read_csv("nato_phonetic_alphabet.csv")
 # Create a dict from dataframe where key-value pair are the letters-code.
 nato_alphabet = {row["letter"]: row["code"] for (i, row) in data.iterrows()}
 
-user_input = input("What is your name bruv? ")
+user_input = input("Enter a word: ")
 # Create a list of the phonetic code words from a word that the user inputs.
 nato_name = [nato_alphabet[letter.upper()] for letter in user_input]
 
