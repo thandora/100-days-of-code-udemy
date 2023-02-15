c33c2d8df44cf548ed27b1143af4f1e3622e6cbd
Christian Klint Labadia <cklabadia@hotmail.com>
2023-02-15 20:25:21 +0800

    Updated data structure for storing birthdays New structure allows people sharing a birthday.

--- day-032/birthday-wisher/main.py
+++ day-032/birthday-wisher/main.py
@@ -2,6 +2,7 @@ import datetime as dt
 import pandas as pd
 import random
 import smtplib
+from pprint import pprint
 
 """
 Sends a birthday greeting to a celebrant if it's their birthday today.
@@ -9,50 +10,52 @@ The program takes a random template from letter_templates/
 
 """
 MY_EMAIL = "MY_EMAIL"
-APP_PASSWORD = "MY_PASS"
+APP_PASSWORD = "MY_APP_PASS"
 
-now = dt.datetime.now()
 # Today's month and day
+now = dt.datetime.now()
 now_md = (now.month, now.day)
 
 birthdays_df = pd.read_csv("birthdays.csv")
 birthdays = {}
 celebrants = []
 
-# Create a dict for birthdays. (Redundant, but still getting the hang of pandas)
-for row in birthdays_df.iterrows():
-    name = row[1][0]
-    month = row[1][3]
-    day = row[1][4]
-    email = row[1][1]
-    birthdays[name] = (month, day, email)
+# Create a dict for birthdays in the form:
+# {
+#  (month, day): [celebrant1, celebrant2],
+# }
+birthdays = {}
+for _, info in birthdays_df.iterrows():
+    birthday = (info["month"], info["day"])
 
-for data in birthdays.items():
-    name = data[0]
-    birthdate = data[1][0:2]
-    email = data[1][2]
+    if birthday in birthdays:
+        birthdays[birthday].append(info)
+    else:
+        birthdays[birthday] = [info]
 
-    if now_md == birthdate:
-        celebrants.append(name)
+celebrants = birthdays.get(now_md)
 
 # Check if anyone is having their birthday today.
 # "if celebrants" is shorter, but this is arguably more readable.
 if len(celebrants) > 0:
 
-    # Send greetings for each celbrant
+    # Send greetings for each celebrant
     for celebrant in celebrants:
         template_path = "letter_templates/letter_" + str(random.randint(1, 3)) + ".txt"
+
         with open(template_path) as f:
             template = f.read()
-            birthday_message = template.replace("[NAME]", celebrant)
+            birthday_message = template.replace("[NAME]", celebrant["name"])
 
         message = f"Subject:Happy Birthday!\n\n{birthday_message}"
-        celebrant_email = birthdays[celebrant][2]
 
         with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
             connection.starttls()
             connection.login(user=MY_EMAIL, password=APP_PASSWORD)
             connection.sendmail(
-                from_addr=MY_EMAIL, to_addrs=celebrant_email, msg=birthday_message
+                from_addr=MY_EMAIL, to_addrs=celebrant["email"], msg=message
             )
-            print(f"Sent greeting to {celebrant} ({celebrant_email})")
+
+            print(f"Sent greeting to {celebrant['name']} ({celebrant['email']})")
+    i = len(celebrants)
+    print(f"Sent all greetings. ({i}/{i})")
