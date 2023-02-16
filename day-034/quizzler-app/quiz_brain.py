764b04ced86d195919ce1cf8dfc27e63db06e350
Christian Klint Labadia <cklabadia@hotmail.com>
2023-02-16 20:05:19 +0800

    First working version

--- day-034/quizzler-app/quiz_brain.py
+++ day-034/quizzler-app/quiz_brain.py
@@ -1,27 +1,27 @@
-class QuizBrain:
+import html
 
+
+class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.score = 0
         self.question_list = q_list
         self.current_question = None
+        self.n_questions = len(q_list)
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
 
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
+        question_text = html.unescape(self.current_question.text)
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {self.current_question.text} (True/False): ")
-        self.check_answer(user_answer)
+        question_prompt = f"Q: {self.question_number}/{self.n_questions}\n{question_text}"
+        return question_prompt
 
-    def check_answer(self, user_answer):
+    def check_answer(self, user_answer: str) -> bool:
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
-            self.score += 1
-            print("You got it right!")
+            return True
         else:
-            print("That's wrong.")
-
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+            return False
--- day-034/quizzler-app/ui.py
+++ day-034/quizzler-app/ui.py
@@ -1,2 +1,88 @@
-THEME_COLOR = "#375362"
+import tkinter as tk
+from quiz_brain import QuizBrain
 
+THEME_COLOR = "#375362"
+CORRECT_COLOR = "#81DB46"
+WRONG_COLOR = "#DB4646"
+QUESTION_FONT = ("Arial", 18)
+
+
+class QuizInterface:
+    def __init__(self, quiz_brain: QuizBrain) -> None:
+        self.quiz_brain = quiz_brain
+        self.n_questions = quiz_brain.n_questions
+        self.score = 0
+        self.window = tk.Tk()
+        self.window.title("Quizzler")
+        self.window.config(bg=THEME_COLOR, padx=25, pady=25)
+
+        # Canvas
+        self.canvas = tk.Canvas()
+        self.canvas.config(width=300, height=250)
+        self.canvas.grid(row=1, column=0, columnspan=3, pady=(15, 30))
+        self.text_question = self.canvas.create_text(
+            150, 125, text="Hello whirled", font=QUESTION_FONT, width=285
+        )
+
+        # Buttons
+        true_img = tk.PhotoImage(file="images/true.png")
+        false_img = tk.PhotoImage(file="images/false.png")
+
+        self.true_button = tk.Button(
+            image=true_img,
+            borderwidth=0,
+            highlightthickness=0,
+            command=self.true_pressed,
+        )
+
+        self.true_button.grid(row=2, column=0)
+
+        self.false_button = tk.Button(
+            image=false_img,
+            borderwidth=0,
+            highlightthickness=0,
+            command=self.false_pressed,
+        )
+        self.false_button.grid(row=2, column=2)
+
+        # Label
+        self.score_label = tk.Label(
+            text=f"Score: {self.score}/{self.n_questions}",
+            fg="white",
+            bg=THEME_COLOR,
+            font=("Arial", 15),
+        )
+        self.score_label.grid(row=0, column=1)
+        self.next_question()
+        self.window.mainloop()
+
+    def add_score(self):
+        self.score += 1
+        self.score_label.config(text=f"Score: {self.score}/{self.n_questions}")
+
+    def next_question(self) -> None:
+        question = self.quiz_brain.next_question()
+        self.canvas.config(bg="white")
+        self.canvas.itemconfig(self.text_question, text=question, font=QUESTION_FONT)
+
+    def true_pressed(self) -> None:
+        is_correct = self.quiz_brain.check_answer("True")
+        if is_correct:
+            self.add_score()
+        self.canvas_feedback(is_correct=is_correct)
+
+    def false_pressed(self) -> None:
+        is_correct = self.quiz_brain.check_answer("False")
+        if is_correct:
+            self.add_score()
+
+        self.canvas_feedback(is_correct=is_correct)
+
+    def canvas_feedback(self, is_correct: bool) -> None:
+        if is_correct:
+            color = CORRECT_COLOR
+        else:
+            color = WRONG_COLOR
+
+        self.canvas.config(bg=color)
+        self.window.after(1000, self.next_question)
