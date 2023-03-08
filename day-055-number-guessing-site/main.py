3a74523606c7e3e0bbd7c6537ff1e46eee3bc745
Christian Klint Labadia <cklabadia@hotmail.com>
2023-03-08 08:02:14 +0800

    First working version

--- day-055-number-guessing-site/main.py
+++ day-055-number-guessing-site/main.py
@@ -1,80 +1,46 @@
-from flask import Flask, render_template
+"""A simple number guessing game (1-10, inclusive) where the user guesses
+by typing out the guess to the url (e.g. if user guesses 4, they will input
+http://127.0.0.1:5000/4)"""
+
+
+from flask import Flask
+import random
+
 
 app = Flask(__name__)
-
-# congrats = 'https://media.giphy.com/media/xT8qBepJQzUjXpeWU8/giphy.gif'
-# try_again = 'https://media.giphy.com/media/CoND5j6Bn1QZUgm1xX/giphy.gif'
 
 
 @app.route("/")
 def hello_world():
     return (
-        "<h1 style='color: red'>Hello</h1><p>World!</p>"
-        "<img src='https://media.giphy.com/media/xT8qBepJQzUjXpeWU8/giphy.gif' alt='congrats img'>"
+        "<h1 style='color: red'>Guess the number! (1 - 10)</h1>"
+        "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif' alt='intro img'>"
     )
 
 
-# ####### Style functions #########
-def make_bold(func):
-    def wrapper():
-        text = func()
-        return f"<b>{text}</b>"
-
-    return wrapper
+random_number = random.randint(1, 10)
 
 
-def make_emphasis(func):
-    def wrapper():
-        text = func()
-        return f"<em>{text}</em>"
+@app.route("/<int:guess>")
+def show_guess_page(guess):
+    if guess < random_number:
+        message = (
+            f"<h1>{guess} is too low. Try again!</h1>"
+            "<img src='https://media.giphy.com/media/3oEduPoyJdFYgHx7YQ/giphy.gif' alt='low img'>"
+        )
 
-    return wrapper
+    elif guess > random_number:
+        message = (
+            f"<h1>{guess} is too high. Try again!</h1>"
+            "<img src='https://media.giphy.com/media/LKTTAzGboJGzC/giphy.gif' alt='high img'>"
+        )
 
-
-def make_underlined(func):
-    def wrapper():
-        text = func()
-        return f"<u>{text}</u>"
-
-    return wrapper
-
-
-# ################
-
-
-# # Coding exercise
-@app.route("/bye")
-@make_bold
-@make_underlined
-@make_emphasis
-def bye():
-    return "Bye!"
-
-
-# #########
-
-#########################
-@app.route("/correct")
-def congratulate():
-    pass
-
-
-@app.route("/incorrect")
-def incorrect():
-    pass
-
-
-#########################
-
-
-@app.route("/about")
-def about():
-    return render_template("index.html")
-
-
-@app.route("/<name>")
-def greet(name):
-    return f"Hello, {name}"
+    else:
+        message = (
+            "<h1>Congratulations! You got it!</h1>"
+            "<img src='https://media.giphy.com/media/cXblnKXr2BQOaYnTni/giphy.gif' alt='congrats img'>"
+        )
+    return message
 
 
 if __name__ == "__main__":
