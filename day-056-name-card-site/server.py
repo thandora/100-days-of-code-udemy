6698dbd4efde2f5dc6803c19b9c41c9b968169f1
Christian Klint Labadia <cklabadia@hotmail.com>
2023-03-08 11:02:59 +0800

    Create

--- day-056-name-card-site/server.py
+++ day-056-name-card-site/server.py
@@ -8,10 +8,5 @@ def show_home():
     return render_template("index.html")
 
 
-# @app.route("/resume")
-# def show_resume():
-#     return render_template("resume.htm")
-
-
 if __name__ == "__main__":
     app.run(debug=True)
