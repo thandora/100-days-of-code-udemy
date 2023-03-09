471a4142f1d4608cbcb4d7a46ac432068c0f8bcc
Christian Klint Labadia <cklabadia@hotmail.com>
2023-03-09 16:22:08 +0800

    Create Post class for storing blog posts

--- day-057-capstone-simple-blog/capstone-blog/main.py
+++ day-057-capstone-simple-blog/capstone-blog/main.py
@@ -1,20 +1,31 @@
 from flask import Flask, render_template
 import requests
+from post import Post
 
 app = Flask(__name__)
 blog_posts_api = "https://api.npoint.io/c790b4d5cab58020d391"
-blog_posts = requests.get(blog_posts_api).json()
+blog_posts_raw = requests.get(blog_posts_api).json()
+
+# Store blog posts as Post objects.
+posts = []
+for post in blog_posts_raw:
+    blog_id = post["id"]
+    title = post["title"]
+    subtitle = post["subtitle"]
+    body = post["body"]
+    blog_post = Post(blog_id=blog_id, title=title, subtitle=subtitle, body=body)
+    posts.append(blog_post)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", blog_posts=blog_posts)
+    return render_template("index.html", blog_posts=posts)
 
 
 @app.route("/post/<int:blog_id>")
 def show_blog(blog_id):
-    for post in blog_posts:
-        if post["id"] == blog_id:
+    for post in posts:
+        if post.id == blog_id:
             blog_post = post
             break
 
--- day-057-capstone-simple-blog/capstone-blog/post.py
+++ day-057-capstone-simple-blog/capstone-blog/post.py
@@ -1,2 +1,6 @@
 class Post:
-    pass
+    def __init__(self, blog_id, title, subtitle, body) -> None:
+        self.id = blog_id
+        self.title = title
+        self.subtitle = subtitle
+        self.body = body
