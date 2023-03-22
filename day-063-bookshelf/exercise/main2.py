8fe07b8a0d609abcd4677fd8d523a69d4145513c
Christian Klint Labadia <cklabadia@hotmail.com>
2023-03-22 15:15:52 +0800

    Added description for autoincrement

--- day-063-bookshelf/exercise/main2.py
+++ day-063-bookshelf/exercise/main2.py
@@ -18,6 +18,7 @@ db.init_app(app)
 class Book(db.Model):
     # autoincrements allow new entries to be assigned new incrementing
     # primary key automatically
+    # This is set to True by default.
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     author = db.Column(db.String(250), nullable=False)
