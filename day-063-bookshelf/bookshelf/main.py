ac59228dda91ae39617e4197c52042c9dbb637dc
Christian Klint Labadia <cklabadia@hotmail.com>
2023-03-22 16:24:59 +0800

    Working version

--- day-063-bookshelf/bookshelf/main.py
+++ day-063-bookshelf/bookshelf/main.py
@@ -1,28 +1,71 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books.db"
+db = SQLAlchemy()
+db.init_app(app)
 
-all_books = []
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float(), nullable=False)
+
+
+# Run only once.
+# Save table to database
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def home():
+    all_books = db.session.query(Book).all()
     return render_template("index.html", books=all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        book_entry = {
-            "title": request.form["book_title"],
-            "author": request.form["book_author"],
-            "rating": float(request.form["book_rating"]),
-        }
-        all_books.append(book_entry)
-        print(all_books)
+        book_entry = Book(
+            title=request.form["book_title"],
+            author=request.form["book_author"],
+            rating=float(request.form["book_rating"]),
+        )
+
+        with app.app_context():
+            db.session.add(book_entry)
+            db.session.commit()
+
         return redirect(url_for("home"))
 
     return render_template("add.html")
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit_rating():
+    if request.method == "POST":
+        book_id = request.form["id"]
+        book_to_update = db.session.get(Book, book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    book_id = request.args.get("id")
+    book = db.session.get(Book, book_id)
+    return render_template("edit_rating.html", book=book)
+
+
+@app.route("/delete")
+def delete_book():
+    book_id = request.args.get("id")
+    book = db.session.get(Book, book_id)
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
