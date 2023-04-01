c48aa3e12e63f96f687e5ae7f39e3d2f70caf0af
Christian Klint Labadia <cklabadia@hotmail.com>
2023-04-01 20:19:39 +0800

    Added flash messages on register or logging in

--- day-068-user-authentication/main.py
+++ day-068-user-authentication/main.py
@@ -43,7 +43,7 @@ class User(UserMixin, db.Model):
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @login_manager.user_loader
@@ -53,48 +53,63 @@ def load_user(user_id):
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    error = None
     if request.method == "POST":
+        email = request.form.get("email")
+        user = User.query.filter_by(email=email).first()
+        if user:
+            error = "An account with that email is already registered."
+            flash(error)
+        else:
+            password = request.form.get("password")
 
-        password = request.form.get("password")
+            secure_pass = generate_password_hash(
+                password=password, method="pbkdf2:sha256", salt_length=8
+            )
+            new_user = User(
+                email=email,
+                name=request.form.get("name"),
+                password=secure_pass,
+            )
 
-        secure_pass = generate_password_hash(
-            password=password, method="pbkdf2:sha256", salt_length=8
-        )
-        new_user = User(
-            email=request.form.get("email"),
-            name=request.form.get("name"),
-            password=secure_pass,
-        )
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
 
-        db.session.add(new_user)
-        db.session.commit()
+            return redirect(url_for("secrets", name=new_user.name))
 
-        return redirect(url_for("secrets", name=new_user.name))
-
-    return render_template("register.html")
+    return render_template(
+        "register.html", error=error, logged_in=current_user.is_authenticated
+    )
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    error = None
 
     if request.method == "POST":
         email = request.form.get("email")
         user = User.query.filter_by(email=email).first()
-
         if user:
             password = user.password
             form_password = request.form.get("password")
             if check_password_hash(pwhash=password, password=form_password):
                 login_user(user, remember=False)
                 return redirect(url_for("secrets"))
+            else:
+                error = "Incorrect email or password."
+                flash(error)
+        else:
+            error = "Incorrect email or password."
+            flash(error)
 
-    return render_template("login.html")
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/secrets")
 @login_required
 def secrets():
-    return render_template("secrets.html", name=current_user.name)
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
 
 
 @app.route("/logout")
@@ -109,15 +124,6 @@ def download():
     return send_from_directory(
         directory=app.static_folder, path="files/cheat_sheet.pdf"
     )
-
-
-@app.route("/click")
-def click():
-    x = User.query.filter_by(email="qqq").first()
-    userera = User.query.filter_by(email="quin@email.com").first()
-
-    print(f"is active: {userera.is_authenticated}")
-    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
