4aae893191d9ee34a0f6945c62ce7901a4a90f8a
Christian Klint Labadia <cklabadia@hotmail.com>
2023-04-02 13:04:54 +0800

    Implemented relational database

--- day-069-capstone-final-blog/forms.py
+++ day-069-capstone-final-blog/forms.py
@@ -1,12 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL
+from wtforms import StringField, SubmitField, PasswordField, EmailField, HiddenField
+from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditorField
 
-##WTForm
+# WTForm
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = HiddenField("Author", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
+
+
+class RegisterForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Register")
+
+
+class LoginForm(FlaskForm):
+    email = EmailField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
--- day-069-capstone-final-blog/main.py
+++ day-069-capstone-final-blog/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
@@ -13,15 +13,21 @@ from flask_login import (
     current_user,
     logout_user,
 )
-from forms import CreatePostForm
+from functools import wraps
+from forms import CreatePostForm, RegisterForm, LoginForm
 
 # from flask_gravatar import Gravatar
+
+
+ADMIN_EMAIL = "a@a.a"
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ckeditor = CKEditor(app)
 Bootstrap(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 # CONNECT TO DB
@@ -30,11 +36,44 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
+# Admin-only decorator
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # Abort if logged in used is not admin.
+        if current_user.email != ADMIN_EMAIL:
+            return abort(403)
+
+        # Otherwise continue with the route function
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
 # CONFIGURE TABLES
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), unique=True, nullable=False)
+
+    # The "author" refers to the author property in the BlogPost class.
+    posts = relationship("BlogPost", back_populates="author")
+
+
 class BlogPost(db.Model):
     __tablename__ = "blog_posts"
+
     id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.String(250), nullable=False)
+
+    # Create Foreign Key, "users.id" the users refers to the tablename of User.
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    # Create reference to the User object, the "posts" refers to the posts
+    # property in the User class.
+    author = relationship("User", back_populates="posts")
+
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
@@ -42,28 +81,79 @@ class BlogPost(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
 
 
-with app.app_context():
-    db.create_all()
+# # Run once. Create tables in database
+# with app.app_context():
+#     db.create_all()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.get(User, user_id)
 
 
 @app.route("/")
 def get_all_posts():
     posts = BlogPost.query.all()
-    return render_template("index.html", all_posts=posts)
+    return render_template(
+        "index.html", all_posts=posts, logged_in=current_user.is_authenticated
+    )
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    form = RegisterForm()
+    error = None
+
+    if form.validate_on_submit():
+        email = form.email.data
+        user = User.query.filter_by(email=email).first()
+
+        if user is None:
+            new_user = User()
+            form.populate_obj(new_user)
+            new_user.password = generate_password_hash(
+                form.password.data, method="pbkdf2:sha256", salt_length=8
+            )
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            login_user(new_user)
+            return redirect(url_for("get_all_posts"))
+        else:
+            error = "Email is already registered with another account."
+            flash(error)
+            return redirect(url_for("login"))
+
+    return render_template(
+        "register.html", form=form, logged_in=current_user.is_authenticated
+    )
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    error = None
+
+    if form.validate_on_submit():
+        email = form.email.data
+        user = User.query.filter_by(email=email).first()
+        if user is not None:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for("get_all_posts"))
+
+        error = "Incorrect credentials"
+        flash(error)
+
+    return render_template(
+        "login.html", form=form, logged_in=current_user.is_authenticated
+    )
 
 
 @app.route("/logout")
 def logout():
+    logout_user()
     return redirect(url_for("get_all_posts"))
 
 
@@ -83,7 +173,7 @@ def contact():
     return render_template("contact.html")
 
 
-@app.route("/new-post")
+@app.route("/new-post", methods=["GET", "POST"])
 def add_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
@@ -101,7 +191,7 @@ def add_new_post():
     return render_template("make-post.html", form=form)
 
 
-@app.route("/edit-post/<int:post_id>")
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
     edit_form = CreatePostForm(
@@ -115,7 +205,7 @@ def edit_post(post_id):
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
-        post.author = edit_form.author.data
+        post.author = current_user
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
@@ -128,6 +218,17 @@ def delete_post(post_id):
     post_to_delete = BlogPost.query.get(post_id)
     db.session.delete(post_to_delete)
     db.session.commit()
+    return redirect(url_for("get_all_posts"))
+
+
+@app.route("/debug-me")
+@login_required
+@admin_only
+def debug_me():
+    # x = User.query.filter_by(email="qqq").first()
+    # aaaaa = User.query.filter_by(email="a@a.a").first()
+
+    # print(f">>>>is active: {current_user.is_authenticated}")
     return redirect(url_for("get_all_posts"))
 
 
